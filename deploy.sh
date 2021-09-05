c1094915a12b291bdd0f2c39a86f1168d4124916
yaopengkun <flyingfish8@aliyun.com>
2021-09-05 10:31:44 +0800

    feat: tijiao

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,13 @@ cd docs/.vuepress/dist
 # 为github提供自定义域名解析
 echo "blog.flyingfish8.cn" > CNAME
 
+currentdate=$(date +%Y%m%d)
+
 git init
 
 git add -A  
 
-git commit -m 'deploy'
+git commit -m 'deploy::'$currentdate
 
 git push -f git@github.com:flyingfish888/blog.git master:gh-pages
 
